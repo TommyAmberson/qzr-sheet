3fba3d682ff08862842a2145c0b6417447a41752
Tom Amberson <53712733+TommyAmberson@users.noreply.github.com>
2026-03-28 18:24:39 -0600

    Revert "fix(deploy): revert base to / and simplify _redirects"
    This reverts commit fc28f3f1dc19e0097fbd560713281fe571e6c98e.

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 export default defineConfig({
   plugins: [vue(), vueDevTools()],
-  base: '/',
+  base: '/scoresheet/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
