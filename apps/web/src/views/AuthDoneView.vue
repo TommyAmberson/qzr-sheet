f7fcce06f2bc4f2bd381ae818c8637aaf3c3b620
Tom Amberson <53712733+TommyAmberson@users.noreply.github.com>
2026-03-30 19:50:00 -0600

    feat(web): add /auth/done route to store JWT and redirect home

---
+++ apps/web/src/views/AuthDoneView.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { onMounted } from 'vue'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+onMounted(() => {
+  const params = new URLSearchParams(window.location.search)
+  const token = params.get('token')
+  if (token) {
+    localStorage.setItem('auth_token', token)
+  }
+  router.replace('/')
+})
+</script>
+
+<template>
+  <div />
+</template>
